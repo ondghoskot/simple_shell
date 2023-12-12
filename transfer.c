7e207109bef97ab83c4f5478fdc14c44dd2fae90
Youssef LIZDIHAR <141226579+YoussefLizdihar@users.noreply.github.com>
2023-12-12 18:52:28 +0000

    Update transfer.c

--- transfer.c
+++ transfer.c
@@ -7,42 +7,41 @@
  */
 char **transfer(char *l)
 {
-	char *t = NULL, *temp = NULL, dlm[] = " \t\n";
-	char **c = NULL;
-	int i = 0, j = 0;
+        char *t = NULL, *temp = NULL, dlm[] = " \t\n";
+        char **c = NULL;
+        int i = 0, j = 0;
 
-	if (!l)
-		return (NULL);
-	temp = _strdup(l);
-	t = strtok(temp, dlm);
-	if (!t)
-	{
-		free(l), l = NULL;
-		free(temp), temp = NULL;
-		return (NULL);
-	}
-	while (t)
-	{
-		i++;
-		t = strtok(NULL, dlm);
-	}
-	free(temp);
-	c = malloc(sizeof(char *) * (i + 1));
-	if (c == NULL)
-	{
-		free(l);
-		free(c);
-		l = NULL;
-		return (NULL);
-	}
-	t = strtok(l, dlm);
-	while (t)
-	{
-		c[j] = _strdup(t);
-		t = strtok(NULL, dlm);
-		j++;
-	}
-	free(l);
-	c[j] = NULL;
-	return (c);
+        if (!l)
+                return (NULL);
+        temp = _strdup(l);
+        t = strtok(temp, dlm);
+        if (!t)
+        {
+                free(l), l = NULL;
+                free(temp), temp = NULL;
+                return (NULL);
+        }
+        while (t)
+        {
+                i++;
+                t = strtok(NULL, dlm);
+        }
+        free(temp), temp = NULL;
+        c = malloc(sizeof(char *) * (i + 1));
+        if (c == NULL)
+        {
+                free(l), l = NULL;
+                imFree(c);
+                return (NULL);
+        }
+        t = strtok(l, dlm);
+        while (t)
+        {
+                c[j] = _strdup(t);
+                t = strtok(NULL, dlm);
+                j++;
+        }
+        free(l), l = NULL;
+        c[j] = NULL;
+        return (c);
 }
