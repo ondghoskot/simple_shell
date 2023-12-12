45e207f3dc5dd003fcd7cccddb59615e34f64194
ondghoskot <anaschtioui65@gmail.com>
2023-12-12 01:11:57 +0000

    {updated main function}

---
+++ simple_shell.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - the main function of the S.S project
+ * @ac: number of CL args
+ * @av: CL args array
+ * Return: 0 (Sucess)
+ */
+int main(int ac, char **av)
+{
+	char *l = NULL, **c = NULL;
+	int i = 0, j = 1;
+	(void) ac;
+
+	for (; j > 0; j++)
+	{
+		l = scanner();
+		if (l == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				ppr("\n", 1);
+			return (i);
+		}
+		c = transfer(l);
+		if (!c)
+			continue;
+		i = executer(c, av);
+	}
+	return (0);
+}
